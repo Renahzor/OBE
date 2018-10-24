d1f8d5b97c0a1b5be5caac4a1584c05b7c1b6e58
Renahzor <joshy09090@yahoo.com>
2018-10-23 23:42:20 -0600

    Day 16
    Nothing much done, brainstormed about different ways to store items and efficiently add new ones

--- OBE/Assets/Scripts/NPCScripts/EquipmentItem.cs
+++ OBE/Assets/Scripts/NPCScripts/EquipmentItem.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EquipmentType { Weapon, Armor, Consumable }
+
 public class EquipmentItem : MonoBehaviour {
 
+    public EquipmentType equipmentType { get; private set; }
+
+    int minDamage;
+    int maxDamage;
+    int armorClass;
+
+    int rarity;
+
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
